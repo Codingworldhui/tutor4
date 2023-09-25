8b72be76313bb8df4d6a2bde874a6d767962ab35
huizi <1509736024@qq.com>
2023-09-25 10:30:40 +0800

    “nothing”

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -2,13 +2,9 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
 public class CalcTest {
   Calc c = new Calc();
-
     @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
-    }
-    void testSubtraction(){assertEquals(2, c.subtract(4,2))}
+    void testAddition() {  assertEquals(4, c.add(2,2));}
+    void testSubtraction(){assertEquals(2, c.subtract(4,2));}
 }
